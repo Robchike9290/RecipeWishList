7e2d63f279b432dbd57819492fe21043582eae64
Robert Czajka <robchike9290@gmail.com>
2022-03-15 16:04:06 -0500

    Created test button with test click handler in App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from "react";
 
 const App = () => {
 
-    return (
-      <div>
-        <h1>
-          THIS IS THE RECIPE WISH LIST
-        </h1>
-      </div>
-    );
+  const handleClick = () => {
+    console.log('I AM THE CLICK HANDLER.  WHY HAVE YOU DISTRUBED MY SLUMBER???');
+  }
+
+  return (
+    <div>
+      <h1>
+        THIS IS THE RECIPE WISH LIST
+      </h1>
+      <button onClick={handleClick}>CLICK ME</button>
+    </div>
+  );
 
 }
 
